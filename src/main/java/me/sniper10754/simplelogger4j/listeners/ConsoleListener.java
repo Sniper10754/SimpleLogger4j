a6e66bf3c33c0bdfbd5277073bafe74afd76a53a
Sniper10754 <74132494+Sniper10754@users.noreply.github.com>
2022-02-06 19:55:28 +0100

    Add kind

--- src/main/java/me/sniper10754/simplelogger4j/listeners/ConsoleListener.java
+++ src/main/java/me/sniper10754/simplelogger4j/listeners/ConsoleListener.java
@@ -2,17 +2,48 @@ package me.sniper10754.simplelogger4j.listeners;
 
 import me.sniper10754.simplelogger4j.event.LogEvent;
 
+import java.io.OutputStream;
+
 /**
  * The type Console listener.
  * <p>
  * Prints the event to {@code System.out}
  */
 public class ConsoleListener extends StreamListener {
+    
+    /**
+     * Instantiates a new Console listener.
+     */
+    enum Kind {
+        /**
+         * Err kind.
+         */
+        ERR,
+        /**
+         * Out kind.
+         */
+        OUT
+    }
+    
     /**
      * Instantiates a new Console listener.
      */
     public ConsoleListener() {
-        super(System.out);
+        this(Kind.OUT);
+    }
+    
+    /**
+     * Instantiates a new Console listener.
+     *
+     * @param kind the kind
+     */
+    public ConsoleListener(Kind kind) {
+        this((kind == Kind.OUT) ? System.out :
+             (kind == Kind.ERR) ? System.err : null);
+    }
+
+    private ConsoleListener(OutputStream stream) {
+        super(stream);
     }
     
     @Override
